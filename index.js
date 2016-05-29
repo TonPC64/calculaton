f9eaea58a6f48e2accb5fccd63ff50b03c8e1810
kingkong64 <kingkong2103@gmail.com>
2016-05-29 09:40:27 +0700

    sum

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ var keyword = {
   }
 }
 
-var qustion = ['What Do you have?', 'How much', 'Do you have', 'Add']
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -31,63 +29,61 @@ app.post('/data', jsonParser, function (req, res) {
   var data = req.body
   if (!keyword[data.key]) {
     keyword[data.key] = {
-      ans: data.ans,
+      ans: data.ans
     }
   }
   res.send(keyword)
 })
 
 app.post('/webhook/', function (req, res) {
-  var str = ''
   var messaging_events = req.body.entry[0].messaging
   for (var i = 0; i < messaging_events.length; i++) {
     var event = req.body.entry[0].messaging[i]
     var sender = event.sender.id
     if (event.message && event.message.text) {
-      var text = event.message.text
-      var textSlice = event.message.text.split(' ')
-      console.log(textSlice);
-      /*
-      var textSlice = event.message.text.split(' ')
-      var thig = textSlice[textSlice.length - 1].split('?')[0]
-      console.log(thig, qustion)
-      if (text === qustion[0]) {
-        str = 'Have '
-        Object.keys(refrigerator).forEach(function (item) {
-          str += item + ', '
-        })
-        sendTextMessage(sender, str)
-      }else if (textSlice[0] + ' ' + textSlice[1] === qustion[1]) {
-        console.log(thig)
-        if (refrigerator[thig]) {
-          sendTextMessage(sender, refrigerator[thig].amount + ' ' + refrigerator[thig].unit)
-        } else {
-          sendTextMessage(sender, "Don't Have")
-        }
-      }else if (textSlice[0] + ' ' + textSlice[1] + ' ' + textSlice[2] === qustion[2]) {
-        console.log(thig)
-        if (refrigerator[thig]) {
-          sendTextMessage(sender, 'Have ' + refrigerator[thig].amount + ' ' + refrigerator[thig].unit)
-        } else {
-          sendTextMessage(sender, "Don't Have")
-        }
-      }else if (textSlice[0] === qustion[3]) {
-        console.log(thig)
-        if (!refrigerator[textSlice[1]]) {
-          refrigerator[textSlice[1]] = {
-            amount: textSlice[2],
-            unit: textSlice[3]
-          }
-        } else {
-          refrigerator[textSlice[1]].amount = parseInt(refrigerator[textSlice[1]].amount, 0) + parseInt(textSlice[2], 0)
-        }
-        sendTextMessage(sender, 'Done')
+      var text = event.message.text.split(' ')
+      if (text[0] === 'sum') {
+        var ans = text[1] + text[2]
+        sendTextMessage(sender, ans)
+      }
+    /*
+    var textSlice = event.message.text.split(' ')
+    var thig = textSlice[textSlice.length - 1].split('?')[0]
+    console.log(thig, qustion)
+    if (text === qustion[0]) {
+      str = 'Have '
+      Object.keys(refrigerator).forEach(function (item) {
+        str += item + ', '
+      })
+      sendTextMessage(sender, str)
+    }else if (textSlice[0] + ' ' + textSlice[1] === qustion[1]) {
+      console.log(thig)
+      if (refrigerator[thig]) {
+        sendTextMessage(sender, refrigerator[thig].amount + ' ' + refrigerator[thig].unit)
       } else {
-        console.log(textSlice[0] + ' ' + textSlice[1])
-      }*/
-
-
-
+        sendTextMessage(sender, "Don't Have")
+      }
+    }else if (textSlice[0] + ' ' + textSlice[1] + ' ' + textSlice[2] === qustion[2]) {
+      console.log(thig)
+      if (refrigerator[thig]) {
+        sendTextMessage(sender, 'Have ' + refrigerator[thig].amount + ' ' + refrigerator[thig].unit)
+      } else {
+        sendTextMessage(sender, "Don't Have")
+      }
+    }else if (textSlice[0] === qustion[3]) {
+      console.log(thig)
+      if (!refrigerator[textSlice[1]]) {
+        refrigerator[textSlice[1]] = {
+          amount: textSlice[2],
+          unit: textSlice[3]
+        }
+      } else {
+        refrigerator[textSlice[1]].amount = parseInt(refrigerator[textSlice[1]].amount, 0) + parseInt(textSlice[2], 0)
+      }
+      sendTextMessage(sender, 'Done')
+    } else {
+      console.log(textSlice[0] + ' ' + textSlice[1])
+    }*/
     }
   }
   res.sendStatus(200)
